21c19ed42aa6c2b1d66ea41bae94367e5455d031
xhide341 <shawnehgn10@gmail.com>
2025-03-05 01:13:44 +0800

    chore: add autoprefixer and update theme configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Header from "./components/header";
 import { Clock } from "./components/clock";
 
 export const App = () => {
+  const [theme, setTheme] = useState("forest");
+
   return (
-    <div className="font-roboto bg-background text-foreground mx-auto flex h-dvh w-full max-w-dvw flex-col p-4">
+    <div
+      data-theme={theme}
+      className="font-roboto bg-background text-foreground mx-auto flex h-dvh w-full max-w-dvw flex-col p-4"
+    >
       <div className="mx-auto flex w-full max-w-3xl flex-col">
         <Header />
         <Clock />
