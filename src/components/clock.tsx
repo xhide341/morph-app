af00950458d91aaa9d9683ffaea186c5734ac19d
xhide341 <shawnehgn10@gmail.com>
2025-03-06 01:44:35 +0800

    feat: add reset button

--- src/components/clock.tsx
+++ src/components/clock.tsx
@@ -53,21 +53,28 @@ export const Clock = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="bg-secondary flex flex-col items-center justify-center rounded-xl p-10">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-[8rem] font-bold">{time}</h1>
-        <div className="flex flex-row gap-2 text-center text-2xl">
+        <div className="flex flex-row justify-center gap-4 text-center text-2xl">
           <button
             aria-label={isRunning ? "Pause" : "Start"}
-            className={`flex w-[8rem] items-center gap-2 rounded-md px-4 py-2 text-white transition-all duration-200 ease-in-out hover:cursor-pointer ${
+            className={`flex w-32 min-w-32 items-center justify-center gap-2 rounded-md px-4 py-2 text-white transition-all duration-200 ease-in-out hover:cursor-pointer ${
               isRunning
-                ? "scale-95 transform bg-green-600 shadow-inner"
-                : "bg-blue-600 hover:scale-[0.98] active:scale-95"
+                ? "scale-95 transform bg-green-600 shadow-inner hover:bg-green-900"
+                : "bg-blue-600 hover:scale-[0.98] hover:bg-blue-900 active:scale-95"
             }`}
             onClick={isRunning ? handlePause : handleStart}
           >
             {isRunning ? <Pause size={24} /> : <Play size={24} />}
             {isRunning ? "Pause" : "Start"}
+          </button>
+          <button
+            aria-label="Reset"
+            className="flex w-32 min-w-32 items-center justify-center rounded-md bg-red-600 px-4 py-2 text-white transition-all duration-200 ease-in-out hover:scale-[0.98] hover:cursor-pointer hover:bg-red-900 active:scale-95"
+            onClick={handleReset}
+          >
+            <RotateCcw size={24} />
           </button>
         </div>
       </div>
