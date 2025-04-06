c3b3d38fd7cfa14dec306a6b00303b0dfb946af2
xhide341 <shawnehgn10@gmail.com>
2025-04-07 02:05:43 +0800

    refactor: improve tooltip

--- client/src/components/user-display.tsx
+++ client/src/components/user-display.tsx
@@ -52,31 +52,47 @@ export const UserDisplay = ({ users, roomId }: { users: RoomUser[]; roomId: stri
             onMouseLeave={() => setTooltips((prev) => ({ ...prev, [user.userName]: false }))}
           >
             {user.userName.charAt(0)}
+            {/* tooltip */}
             <div
-              className={`bg-secondary text-foreground absolute -top-8 left-1/2 -translate-x-1/2 rounded px-1.5 py-1 text-xs font-medium whitespace-nowrap transition-all duration-200 ${
+              className={`bg-secondary absolute -top-10 left-1/2 -translate-x-1/2 rounded-xl p-1 transition-all duration-200 ${
                 tooltips[user.userName] ? "visible opacity-100" : "invisible opacity-0"
               }`}
             >
-              {user.userName}
-              <div className="bg-secondary absolute -bottom-1 left-1/2 h-2 w-2 -translate-x-1/2 rotate-45 transform"></div>
+              <div className="text-foreground flex items-center text-xs font-medium whitespace-nowrap">
+                <p className="bg-primary/80 rounded-lg px-1.5 py-1 text-gray-800">
+                  {user.userName}
+                </p>
+              </div>
+              {/* arrow */}
+              <div className="border-t-secondary absolute -bottom-1 left-1/2 h-0 w-0 -translate-x-1/2 border-x-[5px] border-t-[5px] border-solid border-x-transparent"></div>
             </div>
           </div>
         ))}
-        {/* mini modal on hover */}
         <div className="relative flex flex-col items-center justify-center gap-1">
+          {/* tooltip */}
           <div
-            className={`bg-secondary text-foreground absolute -top-8 left-1/2 flex -translate-x-1/2 rounded px-1.5 py-1 text-xs font-medium whitespace-nowrap transition-none ${
+            className={`bg-secondary absolute -top-10 left-1/2 -translate-x-1/2 rounded-xl p-1 pr-1.5 text-xs transition-all duration-200 ${
               tooltips["add"] ? "visible opacity-100" : "invisible opacity-0"
             }`}
             onMouseEnter={() => setTooltips((prev) => ({ ...prev, add: true }))}
             onMouseLeave={() => setTooltips((prev) => ({ ...prev, add: false }))}
           >
-            {roomUrl}
-            <Copy className="hover:text-primary ml-1 h-4 w-4 cursor-pointer" onClick={handleCopy} />
-            <div className="bg-secondary absolute -bottom-1 left-1/2 h-2 w-2 -translate-x-1/2 rotate-45 transform"></div>
+            {/* Inner div with white background */}
+            <div className="flex items-center text-xs font-thin whitespace-nowrap text-gray-800">
+              <p className="bg-primary/80 rounded-lg px-1.5 py-1">{roomUrl}</p>
+              <Copy
+                className={`ml-1 h-4 w-4 transform cursor-pointer transition-all ${
+                  copied ? "text-white/80" : "text-white/50 hover:scale-105 hover:text-white/80"
+                }`}
+                onClick={handleCopy}
+              />
+            </div>
+            {/* arrow */}
+            <div className="border-t-secondary absolute -bottom-1 left-1/2 h-0 w-0 -translate-x-1/2 border-x-[5px] border-t-[5px] border-solid border-x-transparent"></div>
           </div>
+          {/* add button */}
           <div
-            className="bg-background text-foreground border-foreground flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border border-dashed text-lg font-bold transition-all duration-200"
+            className="bg-background text-foreground border-foreground flex h-10 w-10 items-center justify-center rounded-full border border-dashed text-lg font-bold"
             onMouseEnter={() => setTooltips((prev) => ({ ...prev, add: true }))}
             onMouseLeave={() => setTooltips((prev) => ({ ...prev, add: false }))}
           >
