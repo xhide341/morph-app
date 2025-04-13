06fcccc79173720aa5f7df20856ebb453cc82f4b
xhide341 <shawnehgn10@gmail.com>
2025-04-13 13:44:55 +0800

    chore: remove unused code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
